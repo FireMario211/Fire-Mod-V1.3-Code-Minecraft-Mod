83862660c6d68c6f1f5e909d3189ff0f2aa62823
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2016-03-06 17:49:03 -0500

    Create ItemFireShovel.java

---
+++ com/enjin/SurvivalFlame/firemod/item/ItemFireShovel.java
@@ -0,0 +1,12 @@
+package com.enjin.SurvivalFlame.firemod.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemSpade;
+
+public class ItemFireShovel extends ItemSpade {
+
+	public ItemFireShovel(ToolMaterial material) {
+		super(material);
+	}
+
+}
