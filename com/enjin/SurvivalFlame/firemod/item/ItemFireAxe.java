a970211bc7538f30f4e807c0778de07d0c15e29b
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2016-03-06 17:17:45 -0500

    Create ItemFireAxe.java

---
+++ com/enjin/SurvivalFlame/firemod/item/ItemFireAxe.java
@@ -0,0 +1,12 @@
+package com.enjin.SurvivalFlame.firemod.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemAxe;
+
+public class ItemFireAxe extends ItemAxe {
+
+	public ItemFireAxe(ToolMaterial material) {
+		super(material);
+	}
+
+}
