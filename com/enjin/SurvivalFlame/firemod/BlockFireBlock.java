2156cfd7f3be0a94273fca446a17bb76773dd3fa
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2016-03-06 17:13:36 -0500

    Create BlockFireBlock.java

---
+++ com/enjin/SurvivalFlame/firemod/BlockFireBlock.java
@@ -0,0 +1,12 @@
+package com.enjin.SurvivalFlame.firemod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockFireBlock extends Block {
+
+	protected BlockFireBlock(Material material) {
+		super(material);
+	}
+
+}
