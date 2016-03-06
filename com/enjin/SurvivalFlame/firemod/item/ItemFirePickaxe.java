66cadac22ac235bf4cfc6bc3a4f19a268c11407f
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2016-03-06 17:38:34 -0500

    Create ItemFirePickaxe.java

---
+++ com/enjin/SurvivalFlame/firemod/item/ItemFirePickaxe.java
@@ -0,0 +1,12 @@
+package com.enjin.SurvivalFlame.firemod.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemPickaxe;
+
+public class ItemFirePickaxe extends ItemPickaxe {
+
+	public ItemFirePickaxe(ToolMaterial material) {
+		super(material);
+	}
+
+}
