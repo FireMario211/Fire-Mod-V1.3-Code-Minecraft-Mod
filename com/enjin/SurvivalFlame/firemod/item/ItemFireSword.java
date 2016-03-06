59d36a6ced05bf1bca841520f4a237bea238832e
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2016-03-06 17:49:32 -0500

    Create ItemFireSword.java

---
+++ com/enjin/SurvivalFlame/firemod/item/ItemFireSword.java
@@ -0,0 +1,12 @@
+package com.enjin.SurvivalFlame.firemod.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
+
+public class ItemFireSword extends ItemSword {
+
+	public ItemFireSword(ToolMaterial material) {
+		super(material);
+	}
+
+}
